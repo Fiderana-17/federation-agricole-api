74b7ca9f6a93ca48c671eee7d92769d278095560
Fiderana-17 <hei.fiderana@gmail.com>
2026-04-17 10:21:07 +0300

    initialize commit

---
+++ federation-api/src/main/java/com/hei/federation_api/FederationApiApplication.java
@@ -0,0 +1,13 @@
+package com.hei.federation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FederationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FederationApiApplication.class, args);
+	}
+
+}
---
+++ federation-api/src/test/java/com/hei/federation_api/FederationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.federation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FederationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
