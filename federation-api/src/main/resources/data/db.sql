d741c4ead9a42ca5919eee736fbf32470c8cfedf
mena <tsikiniainatsilavina@gmail.com>
2026-04-21 16:03:45 +0300

    feat(db.sql) : delete

--- federation-api/src/main/resources/data/db.sql
+++ federation-api/src/main/resources/data/db.sql
@@ -5,11 +5,3 @@ CREATE USER agricultural_federation_db_manager WITH PASSWORD '123456';
 GRANT CONNECT ON DATABASE agricultural_federation_db TO agricultural_federation_db_manager;
 
 \c agricultural_federation_db
-
-GRANT CREATE ON SCHEMA public TO agricultural_federation_db_manager;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO agricultural_federation_db_manager;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-    GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO agricultural_federation_db_manager;
