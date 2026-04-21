1053445ba53ee2f877ad7cbc70b065cc1ab758d9
mena <tsikiniainatsilavina@gmail.com>
2026-04-21 16:48:42 +0300

    feat(Collectivity.java) : add elements to CollectivityStructure

---
+++ federation-api/src/main/java/com/hei/federation_api/Entity/CollectivityStructure.java
@@ -0,0 +1,7 @@
+public class CollectivityStructure {
+    private String collectivityId;
+    private String presidentId;
+    private String vicePresidentId;
+    private String treasurerId;
+    private String secretaryId;
+}
