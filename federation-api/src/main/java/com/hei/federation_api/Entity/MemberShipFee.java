7c04deae65ade81a1cd501e1c89f2babf925d32d
Fiderana-17 <hei.fiderana@gmail.com>
2026-04-22 11:51:16 +0300

    feat: implement member ship

---
+++ federation-api/src/main/java/com/hei/federation_api/Entity/MemberShipFee.java
@@ -0,0 +1,14 @@
+package com.hei.federation_api.Entity;
+
+public class MemberShipFee {
+
+    public String id;
+    public String eligibleFrom;
+    public String frequency;
+    public Double amount;
+    public String label;
+    public String status;
+    public String collectivityId;
+
+    public MemberShipFee() {}
+}
