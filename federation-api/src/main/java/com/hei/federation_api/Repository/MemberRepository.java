00bd4b5856b8d18aad0c6cc9a77e7ce6ce50b518
Fiderana-17 <hei.fiderana@gmail.com>
2026-04-22 09:46:12 +0300

    feat(member): implement member

--- federation-api/src/main/java/com/hei/federation_api/Repository/MemberRepository.java
+++ federation-api/src/main/java/com/hei/federation_api/Repository/MemberRepository.java
@@ -1,14 +1,7 @@
 package com.hei.federation_api.Repository;
 
-import com.hei.federation_api.Config.DataSource;
-import com.hei.federation_api.Entity.Member;
-
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
+import javax.sql.DataSource;
+import java.sql.*;
 
 public class MemberRepository {
 
@@ -18,104 +11,31 @@ public class MemberRepository {
         this.dataSource = dataSource;
     }
 
-    public void insert(Member member) {
-
+    public boolean existsById(String id) {
         try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement("""
+                SELECT COUNT(*) FROM members WHERE id = ?
+            """);
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1) > 0;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public void insert(com.hei.federation_api.Entity.Member m) {
+        try (Connection conn = dataSource.getConnection()) {
             PreparedStatement ps = conn.prepareStatement("""
                 INSERT INTO members(id, first_name, last_name, email)
                 VALUES (?, ?, ?, ?)
             """);
-
-            ps.setString(1, member.id);
-            ps.setString(2, member.firstName);
-            ps.setString(3, member.lastName);
-            ps.setString(4, member.email);
-
+            ps.setString(1, m.id);
+            ps.setString(2, m.firstName);
+            ps.setString(3, m.lastName);
+            ps.setString(4, m.email);
             ps.executeUpdate();
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public boolean existsById(String id) {
-
-        try (Connection conn = dataSource.getConnection()) {
-
-            PreparedStatement ps = conn.prepareStatement("""
-                SELECT COUNT(*) FROM members WHERE id = ?
-            """);
-
-            ps.setString(1, id);
-
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-                return rs.getInt(1) > 0;
-            }
-
-            return false;
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public Member findById(String id) {
-
-        try (Connection conn = dataSource.getConnection()) {
-
-            PreparedStatement ps = conn.prepareStatement("""
-                SELECT id, first_name, last_name, email
-                FROM members
-                WHERE id = ?
-            """);
-
-            ps.setString(1, id);
-
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-                Member m = new Member();
-                m.id = rs.getString("id");
-                m.firstName = rs.getString("first_name");
-                m.lastName = rs.getString("last_name");
-                m.email = rs.getString("email");
-
-                return m;
-            }
-
-            return null;
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public List<Member> findAll() {
-
-        List<Member> list = new ArrayList<>();
-
-        try (Connection conn = dataSource.getConnection()) {
-
-            ResultSet rs = conn.createStatement().executeQuery("""
-                SELECT id, first_name, last_name, email
-                FROM members
-            """);
-
-            while (rs.next()) {
-                Member m = new Member();
-                m.id = rs.getString("id");
-                m.firstName = rs.getString("first_name");
-                m.lastName = rs.getString("last_name");
-                m.email = rs.getString("email");
-
-                list.add(m);
-            }
-
-            return list;
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
