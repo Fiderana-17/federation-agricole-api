8500347499a55a26720105e3d60d736d25a01c28
mena <tsikiniainatsilavina@gmail.com>
2026-04-22 08:46:07 +0300

    feat(CollectivityRepository.java): add file change

--- federation-api/src/main/java/com/hei/federation_api/Entity/CollectivityRepository.java
+++ federation-api/src/main/java/com/hei/federation_api/Entity/CollectivityRepository.java
@@ -1,4 +1,34 @@
 package com.hei.federation_api.Entity;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class CollectivityRepository {
+
+    private final DataSource dataSource;
+
+    public CollectivityRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void insert(String id, String location, boolean approval) {
+
+        try (Connection conn = dataSource.getConnection()) {
+
+            PreparedStatement ps = conn.prepareStatement("""
+                INSERT INTO collectivities(id, location, federation_approval)
+                VALUES (?, ?, ?)
+            """);
+
+            ps.setString(1, id);
+            ps.setString(2, location);
+            ps.setBoolean(3, approval);
+
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
