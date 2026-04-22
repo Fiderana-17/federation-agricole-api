9e3076da2c5b0f21fdf0c93821bfdd384dc31aea
Fiderana-17 <hei.fiderana@gmail.com>
2026-04-22 16:00:40 +0300

    feat: implement member payment

---
+++ federation-api/src/main/java/com/hei/federation_api/Entity/CreateMemberPayment.java
@@ -0,0 +1,11 @@
+package com.hei.federation_api.Entity;
+
+public class CreateMemberPayment {
+
+    public Double amount;
+    public String membershipFeeIdentifier;
+    public String accountCreditedIdentifier;
+    public String paymentMode;
+
+    public CreateMemberPayment() {}
+}
---
+++ federation-api/src/main/java/com/hei/federation_api/Entity/MemberPayment.java
@@ -0,0 +1,14 @@
+package com.hei.federation_api.Entity;
+
+public class MemberPayment {
+
+    public String id;
+    public Double amount;
+    public String paymentMode;
+    public String accountCreditedId;
+    public String creationDate;
+    public String memberId;
+    public String membershipFeeId;
+
+    public MemberPayment() {}
+}
