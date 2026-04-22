f1efed0e4d3fe9e2154cd6c2c0a7366b38160560
mena <tsikiniainatsilavina@gmail.com>
2026-04-22 08:45:19 +0300

    feat(CollectivityRepository.java): created file and add class

--- federation-api/src/main/java/com/hei/federation_api/Entity/CreateCollectivityStructure.java
+++ federation-api/src/main/java/com/hei/federation_api/Entity/CreateCollectivityStructure.java
@@ -1,4 +1,10 @@
 package com.hei.federation_api.Entity;
 
+
 public class CreateCollectivityStructure {
+
+    public String president;
+    public String vicePresident;
+    public String treasurer;
+    public String secretary;
 }
