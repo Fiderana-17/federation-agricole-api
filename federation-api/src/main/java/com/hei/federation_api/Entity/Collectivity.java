7de69c03c6cba93959acff3096c04da3ba15feaf
mena <tsikiniainatsilavina@gmail.com>
2026-04-23 09:12:33 +0300

    feat(Collectivity): add methods to Collectivity

--- federation-api/src/main/java/com/hei/federation_api/Entity/Collectivity.java
+++ federation-api/src/main/java/com/hei/federation_api/Entity/Collectivity.java
@@ -1,4 +1,15 @@
 package com.hei.federation_api.Entity;
 
+import java.util.List;
+
 public class Collectivity {
+
+    public String id;
+    public String name;
+    public String number;
+    public String location;
+    public boolean federationApproval;
+    public List<Member> members;
+
+    public Collectivity() {}
 }
