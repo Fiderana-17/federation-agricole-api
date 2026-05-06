c7d988e1d9f1772bdbc26fa166399286ff289b58
mena <tsikiniainatsilavina@gmail.com>
2026-05-06 09:20:03 +0300

    feat/Entity : add new element in CollectivityLocalStatistics

--- federation-api/src/main/java/com/hei/federation_api/Entity/CollectivityLocalStatistics.java
+++ federation-api/src/main/java/com/hei/federation_api/Entity/CollectivityLocalStatistics.java
@@ -1,4 +1,9 @@
 package com.hei.federation_api.Entity;
 
 public class CollectivityLocalStatistics {
+    public MemberDescription memberDescription;
+    public Double earnedAmount;
+    public Double unpaidAmount;
+    public Double assiduityPercentage; // v0.0.7 Bonus 2
+    public CollectivityLocalStatistics() {}
 }
