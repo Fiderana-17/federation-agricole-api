e863b6e1cdb75ece85aca4fa028d1e561ed56314
Fiderana-17 <hei.fiderana@gmail.com>
2026-05-06 13:48:23 +0300

    fix(member): add birthdate in member

--- federation-api/src/main/java/com/hei/federation_api/Entity/Member.java
+++ federation-api/src/main/java/com/hei/federation_api/Entity/Member.java
@@ -1,6 +1,5 @@
 package com.hei.federation_api.Entity;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public class Member {
@@ -8,7 +7,7 @@ public class Member {
     public String id;
     public String firstName;
     public String lastName;
-    public LocalDate birthDate;
+    public String birthDate;  // String au lieu de LocalDate
     public String gender;
     public String address;
     public String profession;
@@ -16,117 +15,43 @@ public class Member {
     public String email;
     public String occupation;
     public String collectivityId;
-
     public List<Member> referees;
 
-    public Member() {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthDate = birthDate;
-        this.gender = gender;
-        this.address = address;
-        this.profession = profession;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.occupation = occupation;
-        this.collectivityId = collectivityId;
-        this.referees = referees;
-    }
+    public Member() {}
 
-    public String getId() {
-        return id;
-    }
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public String getBirthDate() { return birthDate; }
+    public void setBirthDate(String birthDate) { this.birthDate = birthDate; }
 
-    public String getLastName() {
-        return lastName;
-    }
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
 
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
+    public String getProfession() { return profession; }
+    public void setProfession(String profession) { this.profession = profession; }
 
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
+    public Long getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(Long phoneNumber) { this.phoneNumber = phoneNumber; }
 
-    public String getGender() {
-        return gender;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+    public String getOccupation() { return occupation; }
+    public void setOccupation(String occupation) { this.occupation = occupation; }
 
-    public String getAddress() {
-        return address;
-    }
+    public String getCollectivityId() { return collectivityId; }
+    public void setCollectivityId(String collectivityId) { this.collectivityId = collectivityId; }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getProfession() {
-        return profession;
-    }
-
-    public void setProfession(String profession) {
-        this.profession = profession;
-    }
-
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getOccupation() {
-        return occupation;
-    }
-
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
-    }
-
-    public String getCollectivityId() {
-        return collectivityId;
-    }
-
-    public void setCollectivityId(String collectivityId) {
-        this.collectivityId = collectivityId;
-    }
-
-    public List<Member> getReferees() {
-        return referees;
-    }
-
-    public void setReferees(List<Member> referees) {
-        this.referees = referees;
-    }
+    public List<Member> getReferees() { return referees; }
+    public void setReferees(List<Member> referees) { this.referees = referees; }
 }
